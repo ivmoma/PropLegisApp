cbbd4faea9019b5ae72ee5b224a702e0ee67e770
ivmoma <immonge32@gmail.com>
2023-10-04 18:59:47 -0600

    Add project files.

---
+++ PropLegisApp/PropuestaForm.aspx
@@ -0,0 +1,90 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="PropuestaForm.aspx.cs" Inherits="PropLegisApp.PropuestaForm" %>
+
+<asp:Content ID="Content1" runat="server" contentplaceholderid="MainContent">
+    <main>
+        <section class="row">
+            <h1>Registro de Propuestas Legislativas</h1>
+            <p class="lead">Digite su propuesta legislativa. Proporcione la información requerida en los siguientes campos.</p>
+        </section>
+
+        <section>
+            <div class="col" style="margin-top:15px;">
+                <asp:Label ID="lblID" runat="server" Text="Seleccione tipo de identificación:" Font-Bold="True"></asp:Label>
+                <asp:RequiredFieldValidator ID="rfvTipoID" runat="server" ControlToValidate="rbtnTipoID" ErrorMessage="Debe seleccionar tipo de ID"></asp:RequiredFieldValidator>
+                <br />
+                <br />
+                <asp:RadioButtonList ID="rbtnTipoID" runat="server" RepeatDirection="Horizontal" OnSelectedIndexChanged="rbtnTipoID_SelectedIndexChanged">
+                    <asp:ListItem>Nacional</asp:ListItem>
+                    <asp:ListItem>Residente</asp:ListItem>
+                </asp:RadioButtonList>
+                <br />
+                <asp:Label ID="lblIdentificacion" runat="server" Text="Ingrese identificación:" Font-Bold="True"></asp:Label>
+                <asp:RegularExpressionValidator ID="revTipoID" runat="server" ControlToValidate="txtID" ValidationExpression="^[1-9]-\d{4}-\d{4}$" ErrorMessage="Ingrese identificación en formato correcto"></asp:RegularExpressionValidator>
+                <asp:TextBox ID="txtID" runat="server" Width="200px" CssClass="form-control"></asp:TextBox>
+                <br />
+            </div>
+        </section>
+
+        <section>
+            <div class="col" style="margin-top:15px;">
+                <asp:Label ID="lblNombre" runat="server" Text="Nombre Completo:" Font-Bold="True"></asp:Label>
+                <asp:RequiredFieldValidator ID="rfv_Nombre" runat="server" ControlToValidate="txtNombre" ErrorMessage="* Campo obligatorio" ValidationGroup="allvalidation"></asp:RequiredFieldValidator>
+                <asp:TextBox ID="txtNombre" runat="server" ToolTip="Ingrese su nombre completo" CssClass="form-control"></asp:TextBox>
+                <br />
+                <asp:Label ID="lblApellidos" runat="server" Text="Apellidos:" Font-Bold="True"></asp:Label>
+                <asp:RequiredFieldValidator ID="rfvApellidos" runat="server" ControlToValidate="txtApellidos" ErrorMessage="* Campo obligatorio" ToolTip="Ingrese sus dos apellidos"></asp:RequiredFieldValidator>  
+                <asp:TextBox ID="txtApellidos" runat="server" CssClass="form-control"></asp:TextBox>
+            </div>
+        </section>
+
+        <section>
+             <div class="col" style="margin-top:15px;">
+                 <br />
+                 <asp:Label ID="lblContacto" runat="server" Text="Número telefonico:" Font-Bold="True"></asp:Label>
+                 <asp:TextBox ID="txtContacto" runat="server" Width="150px" CssClass="form-control" style="display:inline-block"></asp:TextBox>
+                 <asp:RegularExpressionValidator ID="revContacto" runat="server" ControlToValidate="txtContacto" 
+                     ValidationExpression="^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{2}$"
+                     ErrorMessage="Ingrese telefono en formato correcto"></asp:RegularExpressionValidator>
+                 <asp:Label ID="lblEmail" runat="server" Text="Correo Electrónico:" Font-Bold="True"></asp:Label>
+                 <asp:TextBox ID="txtEmail" runat="server" Width="150px" CssClass="form-control" style="display:inline-block"></asp:TextBox>
+                 <asp:RegularExpressionValidator ID="revEmail" runat="server" ControlToValidate="txtEmail" ValidationExpression="^\S+@\S+$" ErrorMessage="Formato e-mail incorrecto"></asp:RegularExpressionValidator>
+             </div>
+        </section>
+
+        <section>
+            <div class="col" style="margin-top:15px;">
+                
+                <asp:Label ID="lblProvincia" runat="server" Text="Provincia:" Font-Bold="True"></asp:Label>
+                <asp:DropDownList ID="ddlProvincia" runat="server" Width="150px" CssClass="form-control" style="display:inline-block" AutoPostBack="True" OnSelectedIndexChanged="on_dllProvincia_Changed">
+                    <asp:ListItem>Seleccione</asp:ListItem>
+                    <asp:ListItem>San José</asp:ListItem>
+                    <asp:ListItem>Heredia</asp:ListItem>
+                    <asp:ListItem>Alajuela</asp:ListItem>
+                    <asp:ListItem>Cartago</asp:ListItem>
+                    <asp:ListItem>Puntarenas</asp:ListItem>
+                    <asp:ListItem>Guanacaste</asp:ListItem>
+                    <asp:ListItem>Limón</asp:ListItem>
+                </asp:DropDownList>
+
+                <asp:Label ID="lblCanton" runat="server" Text="Cantón:" Font-Bold="True"></asp:Label>
+                <asp:DropDownList ID="ddlCanton" runat="server" Width="150px" CssClass="form-control" style="display:inline-block">
+                </asp:DropDownList>
+                
+            </div>
+        </section>
+
+        <section>
+            <div class="col">
+                <br />
+                <asp:Label ID="lblPropuesta" runat="server" Text="Ingrese su propuesta:" ToolTip="200 caracteres máximo" Font-Bold="True"></asp:Label>
+                <asp:TextBox ID="txtPropuesta" runat="server" TextMode="MultiLine" Width="540px" CssClass="form-control" MaxLength="200" Rows="3"></asp:TextBox>
+                <br />
+                <asp:Button ID="btnLimpiar" runat="server" Text="Limpiar" OnClick="OnLimpiar_Clicked" />
+                <asp:Button ID="btnGuardar" runat="server" Text="Guardar" OnClick="OnClick" />
+            </div>
+        </section>
+       
+    </main>
+    </section>
+</asp:Content>
+
---
+++ PropLegisApp/PropuestaForm.aspx.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using System.Xml;
+
+namespace PropLegisApp
+{
+    public partial class PropuestaForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void rbtnTipoID_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void on_dllProvincia_Changed(object sender, EventArgs e)
+        {
+            if (ddlProvincia.SelectedItem.Text == "Seleccione")
+            {
+                ddlCanton.Items.Clear();
+                ddlCanton.SelectedItem.Text = " ";
+            }
+            else if (ddlProvincia.SelectedItem.Text == "San José")
+            {
+                ddlCanton.Items.Clear();
+                ddlCanton.Items.Add("San José");
+                ddlCanton.Items.Add("Alajuelita");
+                ddlCanton.Items.Add("Coronado");
+                ddlCanton.Items.Add("Acosta");
+                ddlCanton.Items.Add("Tibas");
+                ddlCanton.Items.Add("Mts. Oca");
+                ddlCanton.Items.Add("Turrubares");
+                ddlCanton.Items.Add("Dota");
+                ddlCanton.Items.Add("Curridabat");
+                ddlCanton.Items.Add("Perez Zeledón");
+                ddlCanton.Items.Add("Escazu");
+                ddlCanton.Items.Add("L. Cortez");
+                ddlCanton.Items.Add("Puriscal");
+                ddlCanton.Items.Add("Tarrazú");
+                ddlCanton.Items.Add("Asserí");
+                ddlCanton.Items.Add("Mora");
+                ddlCanton.Items.Add("Goicoechea");
+                ddlCanton.Items.Add("Sta. Ana");
+                ddlCanton.Items.Add("Desamparados");
+                ddlCanton.Items.Add("Moravia");
+            }
+            else if (ddlProvincia.SelectedItem.Text == "Heredia")
+            {
+                ddlCanton.Items.Clear();
+                ddlCanton.Items.Add("Heredia");
+                ddlCanton.Items.Add("Barva");
+                ddlCanton.Items.Add("Sto. Domingo");
+                ddlCanton.Items.Add("Sta. Barbara");
+                ddlCanton.Items.Add("San Rafael");
+                ddlCanton.Items.Add("San Isidro");
+                ddlCanton.Items.Add("Belén");
+                ddlCanton.Items.Add("Flores");
+                ddlCanton.Items.Add("San Pablo");
+                ddlCanton.Items.Add("Sarapiquí");
+            }
+            else if (ddlProvincia.SelectedItem.Text == "Cartago")
+            {
+                ddlCanton.Items.Clear();
+                ddlCanton.Items.Add("Cartago");
+                ddlCanton.Items.Add("Paraíso");
+                ddlCanton.Items.Add("La Unión");
+                ddlCanton.Items.Add("Jiménez");
+                ddlCanton.Items.Add("Turrialba");
+                ddlCanton.Items.Add("Alvarado");
+                ddlCanton.Items.Add("Oreamuno");
+                ddlCanton.Items.Add("El Guarco");
+            }
+            else if (ddlProvincia.SelectedItem.Text == "Alajuela")
+            {
+                ddlCanton.Items.Clear();
+                ddlCanton.Items.Add("Alajuela");
+                ddlCanton.Items.Add("Atenas");
+                ddlCanton.Items.Add("Grecia");
+                ddlCanton.Items.Add("Guatuso");
+                ddlCanton.Items.Add("Los Chiles");
+                ddlCanton.Items.Add("Naranjo");
+                ddlCanton.Items.Add("Orotina");
+                ddlCanton.Items.Add("Palmares");
+                ddlCanton.Items.Add("Poás");
+                ddlCanton.Items.Add("Río Cuarto");
+                ddlCanton.Items.Add("San Carlos");
+                ddlCanton.Items.Add("San Mateo");
+                ddlCanton.Items.Add("San Ramón");
+                ddlCanton.Items.Add("Sarchí");
+                ddlCanton.Items.Add("Upala");
+                ddlCanton.Items.Add("Zarcero");
+            }
+            else if (ddlProvincia.SelectedItem.Text == "Guanacaste")
+            {
+                ddlCanton.Items.Clear();
+                ddlCanton.Items.Add("Liberia");
+                ddlCanton.Items.Add("Nicoya");
+                ddlCanton.Items.Add("Santa Cruz");
+                ddlCanton.Items.Add("Bagaces");
+                ddlCanton.Items.Add("Carrillo");
+                ddlCanton.Items.Add("Cañas");
+                ddlCanton.Items.Add("Abangares");
+                ddlCanton.Items.Add("Tilarán");
+                ddlCanton.Items.Add("Nandayure");
+                ddlCanton.Items.Add("La Cruz");
+                ddlCanton.Items.Add("Hojancha");
+            }
+            else if (ddlProvincia.SelectedItem.Text == "Puntarenas")
+            {
+                ddlCanton.Items.Clear();
+                ddlCanton.Items.Add("Buenos Aires");
+                ddlCanton.Items.Add("Corredores");
+                ddlCanton.Items.Add("Coto Brus");
+                ddlCanton.Items.Add("Garabito");
+                ddlCanton.Items.Add("Golfito");
+                ddlCanton.Items.Add("Montes de Oro");
+                ddlCanton.Items.Add("Monteverde");
+                ddlCanton.Items.Add("Osa");
+                ddlCanton.Items.Add("Parrita");
+                ddlCanton.Items.Add("Puerto Jiménez");
+                ddlCanton.Items.Add("Puntarenas");
+                ddlCanton.Items.Add("Quepos");
+            }
+            else if (ddlProvincia.SelectedItem.Text == "Limón")
+            {
+                ddlCanton.Items.Clear();
+                ddlCanton.Items.Add("Limón");
+                ddlCanton.Items.Add("Pococí");
+                ddlCanton.Items.Add("Siquírres");
+                ddlCanton.Items.Add("Talamanca");
+                ddlCanton.Items.Add("Matina");
+                ddlCanton.Items.Add("Guácimo");
+            }
+        }
+
+        protected void OnClick(object sender, EventArgs e)
+        {
+            // ruta absoluta archivo registro xml
+            string archivo = "C:\\Users\\immon\\Documents\\XML\\registro.xml";
+
+            // obtiene valores desde formulario
+            String tipoID = rbtnTipoID.Text;
+            String identificacion = txtID.Text; 
+            String nombre = txtNombre.Text;
+            String apellidos = txtApellidos.Text;
+            String contacto = txtContacto.Text;
+            String email = txtEmail.Text;
+            String propuesta = txtPropuesta.Text;
+
+            if (!System.IO.File.Exists(archivo))
+            {
+                XmlWriterSettings xmlWrSettings = new XmlWriterSettings();
+                xmlWrSettings.Indent = true;                    // escribe elementos individuales en lineas nuevas e indenta
+                xmlWrSettings.NewLineOnAttributes = true;       // antepone a cada atributo nueva linea y un nivel adicional de sangria
+                using (XmlWriter xmlWriter = XmlWriter.Create(archivo, xmlWrSettings))
+                {
+                    xmlWriter.WriteStartDocument();
+                    xmlWriter.WriteStartElement("Propuestas");     // FIX ME!!! -> verificar elemento
+
+                    xmlWriter.WriteStartElement("Usuario");
+                    xmlWriter.WriteElementString("TipoID", tipoID);
+                    xmlWriter.WriteElementString("Identificación", identificacion);
+                    xmlWriter.WriteElementString("Nombre", nombre);
+                    xmlWriter.WriteElementString("Apellidos", apellidos);
+                    xmlWriter.WriteElementString("Telefono", contacto);
+                    xmlWriter.WriteElementString("Correo", email);
+                    xmlWriter.WriteElementString("Propuesta", propuesta);
+                    xmlWriter.WriteEndElement();
+
+                    xmlWriter.WriteEndElement();
+                    xmlWriter.WriteEndDocument();
+                    xmlWriter.Flush();
+                    xmlWriter.Close();
+                }
+            }
+            else
+            {
+                XDocument xmlDoc = XDocument.Load(archivo);
+                XElement raiz = xmlDoc.Element("Propuestas");
+                IEnumerable<XElement> filas = raiz.Descendants("Usuario");
+                XElement primeraFila = filas.First();
+                primeraFila.AddBeforeSelf(
+                    new XElement("Usuario"),
+                    new XElement("TipoID", tipoID),
+                    new XElement("Identificación", identificacion),
+                    new XElement("Nombre", nombre),
+                    new XElement("Apellidos", apellidos),
+                    new XElement("Telefono", contacto),
+                    new XElement("Correo", email),
+                    new XElement("Propuesta", propuesta));
+                xmlDoc.Save(archivo);
+            }
+        }
+
+        protected void OnLimpiar_Clicked(object sender, EventArgs e)
+        {
+            txtID.Text = string.Empty;
+            txtNombre.Text = string.Empty;
+            txtApellidos.Text = string.Empty;
+            txtContacto.Text = string.Empty;
+            txtEmail.Text = string.Empty;
+            txtPropuesta.Text = string.Empty;
+            ddlProvincia.SelectedIndex = 0;
+        }
+    }
+}
